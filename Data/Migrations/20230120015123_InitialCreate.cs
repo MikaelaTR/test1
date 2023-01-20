17d3efa1afb62925f0d9f3a94aed4f0af01484bd
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2023-01-20 12:50:51 -0500

    Add course and enrollment models, scaffold course controller and views

---
+++ Data/Migrations/20230120015123_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdvancedProjectMVC.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Credits = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Course");
+        }
+    }
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,12 @@
+﻿namespace AdvancedProjectMVC.Models
+{
+    public class Enrollment
+    {
+        public int ID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public double? Grade { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
