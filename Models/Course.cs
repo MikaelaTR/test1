54d6dec12ad3b38237667b5768a9e3f460937d98
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2023-01-19 20:45:52 -0500

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvancedProjectMVC.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string CourseCode { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvancedProjectMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
