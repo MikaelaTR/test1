351ba07c06e398244ae721f8fa38eb95e9eba8e1
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2023-01-25 23:43:48 -0500

    Add TeachingAssignment relation to Instructor model.

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -1,10 +1,12 @@
 ﻿namespace AdvancedProjectMVC.Models
 {
-    public class Instructor
+    public class Instructor : ApplicationUser
     {
         public int InstructorID { get; set; }
         public double Salary { get; set; }
         public string? Office { get; set; }
+
+        public ICollection<TeachingAssignment>? TeachingAssignments { get; set;}
 
     }
 }
