d93ace590e697364315ad90817c891dd3d7ae314
Superintendent-Vergil <jpfarrel@lakeheadu.ca>
2023-02-28 09:16:03 -0500

    -Changed table styling -Changed font size to be bigger and responsive -Styled details, edit, create, delete for courses -Gave student and course views headers

---
+++ Controllers/EnrollmentsController.cs
@@ -0,0 +1,24 @@
+﻿using AdvancedProjectMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedProjectMVC.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EnrollmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Enrollments
+        public async Task<IActionResult> Index()
+        {
+            return _context.Enrollments != null
+                ? View(await _context.Enrollments.ToListAsync())
+                : Problem("Entity set 'ApplicationDbContext.Enrollments'  is null.");
+        }
+    }
+}
---
+++ Controllers/TeachingAssignmentsController.cs
@@ -0,0 +1,26 @@
+﻿using AdvancedProjectMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedProjectMVC.Controllers
+{
+    public class TeachingAssignmentsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public TeachingAssignmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TeachingAssignments
+        public async Task<IActionResult> Index()
+        {
+            return _context.TeachingAssignments != null
+                ? View(await _context.TeachingAssignments.ToListAsync())
+                : Problem("Entity set 'ApplicationDbContext.Enrollments'  is null.");
+        }
+
+    }
+}
