b55072d2d95d98b8869552cd8965dabe66774764
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2023-02-28 20:25:25 -0500

    Change models to use ApplicationUser instead of inherited classes (Student, Admin, Instructor)

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -3,11 +3,12 @@
     public class ChatMessage
     {
         public int Id { get; set; }
-        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public string ApplicationUserId { get; set; }
         public string Content { get; set; }
         public DateTime DatePosted { get; set; }
 
-        public Student Student { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
         public Course Course { get; set; }
     }
 }
