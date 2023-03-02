fa1f16cecfdbed760f7b0842581331b384051b7d
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2023-03-01 23:36:44 -0500

    Fix controller bindings from StudentId to ApplicationUserId

--- Controllers/ChatMessagesController.cs
+++ Controllers/ChatMessagesController.cs
@@ -56,7 +56,7 @@ namespace AdvancedProjectMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,StudentId,Content,DatePosted")] ChatMessage chatMessage)
+        public async Task<IActionResult> Create([Bind("Id,ApplicationUserId,Content,DatePosted")] ChatMessage chatMessage)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace AdvancedProjectMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,StudentId,Content,DatePosted")] ChatMessage chatMessage)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ApplicationUserId,Content,DatePosted")] ChatMessage chatMessage)
         {
             if (id != chatMessage.Id)
             {
