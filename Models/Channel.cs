75ee0b2d2bebd4990183949672ccf63783bfdfd3
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2023-03-03 00:10:46 -0500

    Add Server and Channel models, add Server relationship on ApplicationUser model.

---
+++ Models/Channel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework;
+
+namespace AdvancedProjectMVC.Models
+{
+    public class Channel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ChannelName { get; set; }
+
+        [Required]
+        public int ServerId { get; set; }
+        public Server Server { get; set; }
+
+        public ICollection<ChatMessage> ChatMessages { get; set; }
+    }
+}
