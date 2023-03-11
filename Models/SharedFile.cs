4990a836001829c118c529c39c3183d2de4ff8be
Kelendyl <aibarkov19@gmail.com>
2023-03-11 13:18:23 -0500

    SharedFile model now contains server and channel fields

--- Models/SharedFile.cs
+++ Models/SharedFile.cs
@@ -4,7 +4,10 @@
     {
         public string FileName { get; set; }
         public string FilePath { get; set; }
-        public int GroupId { get; set; }
+        public int ServerID { get; set; }
+        public int ChannelID { get; set; }
+        public string ServerName { get; set; }
+        public string ChannelName { get; set; }
         public string TestFile { get; set; }
     }
 }
