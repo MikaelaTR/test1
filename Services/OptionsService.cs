ba088a72159093e538cfd361a42d36337997d372
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2023-03-23 00:06:52 -0400

    Create service for injecting options for select lists (as per Microsoft docs). Currently unused.

---
+++ Services/OptionsService.cs
@@ -0,0 +1,26 @@
+﻿using AdvancedProjectMVC.Data;
+using AdvancedProjectMVC.Models;
+using Microsoft.CodeAnalysis.Completion;
+
+namespace AdvancedProjectMVC.Services
+{
+    public class OptionsService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OptionsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Course> GetCourses()
+        {
+            List<Course> courses = new List<Course>();
+            foreach(Course c in _context.Courses.ToList()) 
+            {
+                courses.Add(c);
+            }
+            return courses;
+        }
+    }
+}
